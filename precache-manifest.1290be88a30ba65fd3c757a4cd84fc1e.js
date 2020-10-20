1f1d008d53f1e60df9570291f05f2ab2f5aaf03f
kenswot <68135936+kenswot@users.noreply.github.com>
2020-10-21 00:06:30 +0100

    Updates

---
+++ precache-manifest.1290be88a30ba65fd3c757a4cd84fc1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db49b5edcf8eaee42f9c523b0435f6a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "0ec66544f8c5ffcbd62d",
+    "url": "/covid-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "0901c1f28a3574625ce2",
+    "url": "/covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "0ec66544f8c5ffcbd62d",
+    "url": "/covid-tracker/static/js/2.3cef4f0e.chunk.js"
+  },
+  {
+    "revision": "74abecdc3638247e00d3cef20bdbaf4e",
+    "url": "/covid-tracker/static/js/2.3cef4f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0901c1f28a3574625ce2",
+    "url": "/covid-tracker/static/js/main.540d1248.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
